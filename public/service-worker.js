1b9c5489f3d9684483044bb2b54b3b0032e0d624
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-07 16:55:07 -0400

    finished service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,4 +41,42 @@ self.addEventListener("activate", event => {
     );
   });
 
-  
+  self.addEventListener("fetch", event => {
+    if (
+      event.request.method !== "GET" ||
+      !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(fetch(event.request));
+      return;
+    }
+
+    if (event.request.url.includes("/api/images")) {
+        event.respondWith(
+          caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(event.request)
+              .then(response => {
+                cache.put(event.request, response.clone());
+                return response;
+              })
+              .catch(() => caches.match(event.request));
+          })
+        );
+        return;
+      }
+
+      event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+
+          return caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(event.request).then(response => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    });
