6379b105206f2daf9bdfaf81effbb4e40f499f08
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-11 19:41:32 -0400

    completed indexedb functionality

--- public/db.js
+++ public/db.js
@@ -3,7 +3,7 @@ const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    const store = await db.createObjectStore("store", { autoIncrement:true });
+    db.createObjectStore("store", { autoIncrement:true });
   };
 
 request.onsuccess = event => {
@@ -21,10 +21,32 @@ request.onerror = event => {
 function saveRecord(record) {
     const transaction = db.transaction(["store"], "readwrite");
     const store = transaction.objectStore("store");
-  
     store.add(record);
 }
 
 function checkDatabase() {
-    
+    const transaction = db.transaction(["store"], "readonly");
+    const store = transaction.objectStore("store");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response) => response.json())
+            .then(() => {
+                const transaction = db.transaction(["store"], "readwrite");
+                const store = transaction.objectStore("store");
+                store.clear();
+            });
+        }
+    };
 }
+
+window.addEventListener('online', checkDatabase);
