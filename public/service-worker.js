d4cea45da1b09c087acb8031f01c79aa9f158102
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-11 19:56:21 -0400

    corrected file paths on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
-    '/manifest.webmanifest',
-    '/styles.css',
-    '/index.js',
+    'index.html',
+    'manifest.webmanifest',
+    'styles.css',
+    'index.js',
     'db.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png'
 ];
 
 const STATIC_CACHE = "static-cache";
